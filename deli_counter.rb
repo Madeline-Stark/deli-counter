03af6a9de076d18311b794571fa600035a85b5f8
github username <github email address>
2020-09-17 13:28:50 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+require 'pry'
+
+def line(people)
+    if people.empty?
+        puts "The line is currently empty."
+    else 
+        binding.pry
+        positions = people.map.with_index(1) {|person, index| "#{index}. #{person}"} 
+        first_element = "The line is currently:"
+        positions.unshift(first_element)
+        positions_string = positions.join(" ")
+        puts positions_string
+    end
+end
